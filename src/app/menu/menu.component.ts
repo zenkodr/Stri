4b57068a4198db4a44397a79b1514f3a93ae86d8
walid <walid.bouguima@gmail.com>
2020-05-26 00:37:28 +0200

    Clean code in the .ts file

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,25 +1,18 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
   moduleId: module.id,
-  
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
 
-  dish: Dish; 
-  dishErrMess: string; 
   dishes: Dish[];
   errMess: string;
-  featured: Dish;
-
-  constructor(private dishService: DishService,
-    @Inject('baseURL') private baseURL) { }
-
+  constructor(private dishService: DishService) { }
   ngOnInit() {
     this.dishService.getDishes()
       .subscribe(dishes => this.dishes = dishes,
