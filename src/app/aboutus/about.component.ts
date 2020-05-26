96b12ca474a70a78f385dd502cc773456027b744
walid <walid.bouguima@gmail.com>
2020-05-26 13:28:05 +0200

    Fix minor issues; Mainly inject the RouterExtension to enable back routing to the root page

--- src/app/aboutus/about.component.ts
+++ src/app/aboutus/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Leader } from "../shared/leader";
 import { LeaderService } from "../services/leader.service";
+import { RouterExtensions } from "nativescript-angular/router";
 
 @Component({
     selector: 'app-about', 
@@ -12,10 +13,16 @@ export class AboutComponent implements OnInit {
 
     leaders: Leader[];
     errMess: string;
-    constructor(private leaderService: LeaderService) { }
+    constructor(
+      private leaderService: LeaderService, 
+      private RouterExtension: RouterExtensions) { }
     ngOnInit() {
       this.leaderService.getLeaders()
         .subscribe(leaders => this.leaders = leaders,
           errmess => this.errMess = <any> errmess);
     }  
+
+    goBack(){
+      this.RouterExtension.back(); 
+    }
   }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+import { RouterExtensions } from 'nativescript-angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -12,11 +13,14 @@ export class MenuComponent implements OnInit {
 
   dishes: Dish[];
   errMess: string;
-  constructor(private dishService: DishService) { }
+  constructor(private dishService: DishService, private RE: RouterExtensions ) { }
   ngOnInit() {
     this.dishService.getDishes()
       .subscribe(dishes => this.dishes = dishes,
         errmess => this.errMess = <any> errmess);
   }
   
+  goBack(){
+    this.RE.back(); 
+  }
 }
