0928957eeeb295570fc07e2012d3585b57a96771
walid <walid.bouguima@gmail.com>
2020-05-27 13:57:20 +0200

    Inject the favoriteService and check whether the this.dish.id is already in the list or not. If it is in the list we will do nothing but persist and confirm its existence. If it is not in the list we will add it through the addToFavorites() method. By inject the new service we have acces to the rating and the number of comments in that particular dish.id. I took advantage of this to compute the number of comments and the average rating of that dish upon fetching.

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,11 +6,13 @@ import { switchMap } from 'rxjs/operators';
 import { Dish } from '../shared/dish';
 import { Comment } from '../shared/comment';
 import { DishService } from '../services/dish.service';
+import { FavoriteService } from '../services/favorite.service';
 
 @Component({
     selector: 'app-dishdetail',
     moduleId: module.id,
     templateUrl: './dishdetail.component.html',
+    styleUrls: ['./dishdetail.component.css']
   })
   export class DishdetailComponent implements OnInit {
   
@@ -18,20 +20,38 @@ import { DishService } from '../services/dish.service';
     id: number;
     comment: Comment;
     errMess: string;
-  
+    avgstars: string; 
+    numcomments: number; 
+    favorite: boolean;  
+
     constructor(private dishservice: DishService,
       private route: ActivatedRoute,
       private routerExtensions: RouterExtensions,
+      private favoriteService: FavoriteService, 
       @Inject('baseURL') private baseURL) { }
   
     ngOnInit() {
       this.route.params
         .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-        .subscribe(dish => this.dish = dish,
-            errmess => { this.dish = null; this.errMess = <any>errmess; }); 
-    
+        .subscribe(dish => {
+          this.dish = dish
+          this.favorite = this.favoriteService.isFavorite(this.dish.id); 
+          
+          //compute the average of the comments
+          this.numcomments = this.dish.comments.length;           
+          let total =0; 
+          this.dish.comments.forEach(comment =>total += comment.rating); 
+          this.avgstars = (total/this.numcomments).toFixed(2); 
+             },
+            errmess => this.errMess = <any>errmess); 
           }
-  
+
+          addToFavorites(){
+            if(!this.favorite){
+              this.favorite  = this.favoriteService.addFavorite(this.dish.id);
+            }
+          }
+          
     goBack(): void {
       this.routerExtensions.back();
     }
