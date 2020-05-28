201cce1cef89c3e48b761db6652d5e34903697bf
walid <walid.bouguima@gmail.com>
2020-05-28 21:30:33 +0200

    create a method to toggle between the two textFields with one modal(The reservationmodal) using ModalDialogOptions, ModalDialogService and ViewContainerRef

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { TextField } from 'tns-core-modules/ui/text-field';
 import { Validators, FormBuilder, FormGroup} from '@angular/forms';
 import { Switch } from 'tns-core-modules/ui/switch/switch';
+import { ModalDialogService, ModalDialogOptions } from 'nativescript-angular/modal-dialog';
+import { ReservationModalComponent } from '../reservationModal/reservationmodal.component';
 
 @Component({
     selector: 'app-reservation',
@@ -12,7 +14,10 @@ export class ReservationComponent implements OnInit {
 
     reservation: FormGroup;
 
-    constructor(private formBuilder: FormBuilder) {
+    constructor(
+        private formBuilder: FormBuilder, 
+        private modalService: ModalDialogService, 
+        private vcRef: ViewContainerRef) {
 
             this.reservation = this.formBuilder.group({
                 guests: 3,
@@ -45,6 +50,26 @@ export class ReservationComponent implements OnInit {
         let textField = <TextField>args.object;
 
         this.reservation.patchValue({ dateTime: textField.text});
+    }
+
+    createModalView(args) {
+
+        let options: ModalDialogOptions = {
+            viewContainerRef: this.vcRef,
+            context: args,
+            fullscreen: false
+        };
+
+        this.modalService.showModal(ReservationModalComponent, options)
+            .then((result: any) => {
+                if (args === "guest") {
+                    this.reservation.patchValue({guests: result});
+                }
+                else if (args === "date-time") {
+                    this.reservation.patchValue({ dateTime: result});
+                }
+            });
+
     }
 
     onSubmit() {
