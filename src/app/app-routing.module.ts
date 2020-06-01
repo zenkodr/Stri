2e6547e0dbd630e09e9f1677e4b76fb28722cb65
walid <walid.bouguima@gmail.com>
2020-06-01 16:36:02 +0200

    Add new route to configure the authentication page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,19 +12,21 @@ import { ContactComponent } from "./contact/contact.component";
 import { AboutComponent } from "./aboutus/about.component";
 import { FavoritesComponent } from "./favorites/favorites.component";
 import { ReservationComponent } from "./reservation/reservation.component";
+import { UserAuthComponent } from "./userauth/userauth.component";
 
 
 
 /*Start */
 const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "auth", component: UserAuthComponent }, 
     { path: "home", component: HomeComponent }, 
     { path: "menu", component: MenuComponent }, 
-    { path: "dishdetail/:id", component: DishdetailComponent }, 
     { path: "about", component: AboutComponent }, 
     { path: "favorites", component: FavoritesComponent }, 
     { path: "reservation", component: ReservationComponent }, 
-    { path: "contact", component: ContactComponent }, 
+    { path: "contact", component: ContactComponent },
+    { path: "dishdetail/:id", component: DishdetailComponent },  
 ]
 
 @NgModule({
